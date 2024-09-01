e552b1b8e6681a2d159df4b64d2a33f3dddc7dbb
dhandu1980 <dhandapani.rg@gmail.com>
2024-09-01 20:32:05 +0530

    Add simple Node.js application

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestHandler);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
